5a3aa40904cdc1998634bee7dd22a246e64b8c9d
AlessandroCacciaguerra <113693211+AlessandroCacciaguerra@users.noreply.github.com>
2024-08-28 16:05:21 +0200

    Update GUI.java
    Integrated Andrea's modifications

--- GUI.java
+++ GUI.java
@@ -569,49 +569,12 @@ public class GUI extends JFrame {
 		// Btn listeners
 		captionBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+
+				// Didascalia
 				final JDialog captionDialog = new JDialog();
 				captionDialog.setTitle("Didascalia");
 				final JPanel captionPanel = new JPanel(new BorderLayout());
 				final JTextArea captionTxtArea = new JTextArea();
-
-				final JLabel captionLbl = new JLabel("L'intensità del segnale è così rappresentata (dbm: decibel milliwatt)");
-				final JMenuBar colorMenuBar = new JMenuBar();
-				final JMenu colorMenu = new JMenu("Range");
-				final JMenuItem menu0 = new JMenuItem("Da -75 a -90 dbm");
-				final JMenuItem menu1 = new JMenuItem("Da -60 a -75 dbm");
-				final JMenuItem menu2 = new JMenuItem("Da -45 a -60 dbm");
-				final JMenuItem menu3 = new JMenuItem("Da -30 a -45 dbm");
-				final JMenuItem menu4 = new JMenuItem("> -30 dbm");
-				final JLabel captionLblR = new JLabel("R");
-				final JTextField captionTxtR = new JTextField();
-				final JLabel captionLblG = new JLabel("G");
-				final JTextField captionTxtG = new JTextField();
-				final JLabel captionLblB = new JLabel("B");
-				final JTextField captionTxtB = new JTextField();
-				final JLabel captionError = new JLabel();
-				final JButton changeColorBtn = new JButton("Applica");
-
-				GroupLayout colorLayout = new GroupLayout(captionPanel);
-				captionPanel.setLayout(colorLayout);
-				colorLayout.setHorizontalGroup(colorLayout.createSequentialGroup()
-						.addGroup(colorLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-								.addComponent(captionTxtArea))
-						.addGroup(colorLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-								.addComponent(captionLbl).addComponent(colorMenuBar))
-						.addGroup(colorLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-								.addComponent(captionLblR).addComponent(captionTxtR).addComponent(captionLblG).addComponent(captionTxtG).addComponent(captionLblB).addComponent(captionTxtB))
-						.addGroup(colorLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-										.addComponent(captionError).addComponent(changeColorBtn)));
-
-				colorLayout.setVerticalGroup(colorLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
-						.addGroup(colorLayout.createSequentialGroup().addComponent(captionTxtArea))
-						.addGroup(colorLayout.createSequentialGroup().addComponent(captionLbl)
-								.addComponent(colorMenuBar))
-						.addGroup(colorLayout.createSequentialGroup()
-								.addComponent(captionLblR).addComponent(captionTxtR).addComponent(captionLblG).addComponent(captionTxtG).addComponent(captionLblB).addComponent(captionTxtB))
-						.addGroup(colorLayout.createSequentialGroup().addComponent(captionError)
-								.addComponent(changeColorBtn)));
-
 				captionTxtArea.setLineWrap(true);
 				captionTxtArea.setText("Effetto del materiale dell'ostacolo su un segnale radio\r\n"
 						+ "A seconda del materiale, gli ostacoli possono riflettere le onde radio, assorbirle, privandole di una parte della potenza, o non avere alcun effetto sul segnale radio. Tali materiali sono chiamati radiotrasparenti. Più alto è il coefficiente di assorbimento del segnale e più spesso è l'ostacolo, più forte è l'impatto sulla trasmissione radio.\r\n"
@@ -624,44 +587,92 @@ public class GUI extends JFrame {
 						+ "- Calcestruzzo di 100 mm di spessore\r\n" + "- Muratura di 200 mm di spessore\r\n" + "\r\n"
 						+ "Alto\r\n" + "La potenza si riduce di 100 volte\r\n"
 						+ "- Calcestruzzo di 300 mm di spessore\r\n" + "- Calcestruzzo armato di 200 mm di spessore\r\n"
-						+ "- Travi in alluminio e acciaio\r\n" + "\r\n" + "\r\n" + "Di default, l'edificio è composto dal solo piano Terra, ma si possono aggiungere piani superiori o sotterranei\r\n"
-						+ "Su ogni piano, si provvede uno spazio di " + WIDTH/100 + " metri per " + HEIGHT/100 + " in cui riprodurre la pianta dell'edificio; valori illegali saranno considerati 0.\r\n"
+						+ "- Travi in alluminio e acciaio\r\n" + "\r\n" + "\r\n"
+						+ "Di default, l'edificio è composto dal solo piano Terra, ma si possono aggiungere piani superiori o sotterranei\r\n"
+						+ "Su ogni piano, si provvede uno spazio di " + WIDTH / 100 + " metri per " + HEIGHT / 100
+						+ " in cui riprodurre la pianta dell'edificio; valori illegali saranno considerati 0.\r\n"
 						+ "Porte, finestre interne ed esterne od altre aperture fra stanze sono sempre considerate chiuse ai fini della rilevazione del segnale: si ipotizza il segnale minimo nel caso peggiore.\r\n"
-						+ "La precisione massima nel piazzamento di un muro è di " + DIM_SQUARE + " centimetri, sugli assi x e y; non si accettano muri diagonali.\r\n"
+						+ "La precisione massima nel piazzamento di un muro è di " + DIM_SQUARE
+						+ " centimetri, sugli assi x e y; non si accettano muri diagonali.\r\n"
 						+ "Piani strutturati inframmezzati da piani vuoti od incompleti sono tollerati: si presume che le relative planimetrie siano ininfluenti ai fini della simulazione dell'utente.\r\n"
-						+ "\r\n" + "\r\n" + "Per selezionare un campo da abilitare/disabilitare/cancellare è sufficiente indicarne la posizione e premere il relativo pulsante; non serve impostare correttamente il resto dei campi.\r\n"
-						+ "\r\n" + "\r\n" + "Secondo le normative ETSI EN, la potenza di emittenti wireless in un edificio non può superare i 200 milliWatt e la frequenza deve essere compresa nella banda 5150-5350 MegaHertz.\r\n"
-						+ "Gli emittenti devono essere distanziati di almeno " + DIM_SQUARE / 2 + " centimetri ed i muri si possono intersecare ma non compenetrare.\r\n"
+						+ "\r\n" + "\r\n"
+						+ "Per selezionare un campo da abilitare/disabilitare/cancellare è sufficiente indicarne la posizione e premere il relativo pulsante; non serve impostare correttamente il resto dei campi.\r\n"
+						+ "\r\n" + "\r\n"
+						+ "Secondo le normative ETSI EN, la potenza di emittenti wireless in un edificio non può superare i 200 milliWatt e la frequenza deve essere compresa nella banda 5150-5350 MegaHertz.\r\n"
+						+ "Gli emittenti devono essere distanziati di almeno " + DIM_SQUARE / 2
+						+ " centimetri ed i muri si possono intersecare ma non compenetrare.\r\n"
 						+ "Gli angoli di inizio e di fine sono 0 e 360 per antenne omnidirezionali; per antenne direzionali, l'ampiezza è calcolata in senso orario con 0° = 360° = ore 3.\r\n"
-						+ "\r\n" + "Si presume che tutti gli emittenti, direzionali ed omnidirezionali, siano ottimizzati per trasmettere segnali nel piano in cui sono collocati: i segnali propagati verticalmente sono considerati diffusione.\r\n"
+						+ "\r\n"
+						+ "Si presume che tutti gli emittenti, direzionali ed omnidirezionali, siano ottimizzati per trasmettere segnali nel piano in cui sono collocati: i segnali propagati verticalmente sono considerati diffusione.\r\n"
 						+ "Il guadagno rispetto ad un'emittente isotropica si ritiene già incluso nella potenza; se così non fosse, triplicare quanto si intendeva inserire\r\n");
 				final JScrollPane scroll = new JScrollPane(captionTxtArea);
 				scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 				captionTxtArea.setEditable(false);
-				
-				colorMenuBar.add(colorMenu);
+
+				// Colori
+				final JPanel colorPanel = new JPanel();// Panel containing colors btn/lbl
+				GroupLayout colorLayout = new GroupLayout(colorPanel);
+				colorPanel.setLayout(colorLayout);
+
+				final JLabel captionLbl = new JLabel(
+						"L'intensità del segnale è così rappresentata (dbm: decibel milliwatt)");
+				final JMenuBar colorMenuBar = new JMenuBar();
+				final JMenu colorMenu = new JMenu("Range");
+				final JMenuItem menu0 = new JMenuItem("Da -75 a -90 dbm");
+				final JMenuItem menu1 = new JMenuItem("Da -60 a -75 dbm");
+				final JMenuItem menu2 = new JMenuItem("Da -45 a -60 dbm");
+				final JMenuItem menu3 = new JMenuItem("Da -30 a -45 dbm");
+				final JMenuItem menu4 = new JMenuItem("> -30 dbm");
+				colorMenuBar.add(colorMenu); // Setting MenuBar
 				colorMenu.add(menu0);
 				colorMenu.add(menu1);
 				colorMenu.add(menu2);
 				colorMenu.add(menu3);
 				colorMenu.add(menu4);
+				final JLabel captionLblR = new JLabel("R");
+				final JTextField captionTxtR = new JTextField();
+				final JLabel captionLblG = new JLabel("G");
+				final JTextField captionTxtG = new JTextField();
+				final JLabel captionLblB = new JLabel("B");
+				final JTextField captionTxtB = new JTextField();
+				final JButton changeColorBtn = new JButton("Applica");
+				final JLabel captionError = new JLabel();
 
+				colorLayout.setAutoCreateGaps(true);
+				colorLayout.setAutoCreateContainerGaps(true);
+
+				colorLayout.setHorizontalGroup(colorLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+						.addComponent(captionLbl).addComponent(colorMenuBar)
+						.addGroup(colorLayout.createSequentialGroup()
+								.addGroup(colorLayout.createSequentialGroup().addComponent(captionLblR)
+										.addComponent(captionTxtR))
+								.addGroup(colorLayout.createSequentialGroup().addComponent(captionLblG)
+										.addComponent(captionTxtG))
+								.addGroup(colorLayout.createSequentialGroup().addComponent(captionLblB)
+										.addComponent(captionTxtB)))
+						.addGroup(colorLayout.createSequentialGroup().addComponent(changeColorBtn)
+								.addComponent(captionError)));
+
+				colorLayout.setVerticalGroup(
+						colorLayout.createSequentialGroup().addComponent(captionLbl).addComponent(colorMenuBar)
+								.addGroup(colorLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+										.addGroup(colorLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+												.addComponent(captionLblR).addComponent(captionTxtR))
+										.addGroup(colorLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+												.addComponent(captionLblG).addComponent(captionTxtG))
+										.addGroup(colorLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+												.addComponent(captionLblB).addComponent(captionTxtB)))
+								.addGroup(colorLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+										.addComponent(changeColorBtn).addComponent(captionError)));
+
+				// Set Frame
 				captionPanel.add(scroll);
-				captionPanel.add(captionLbl);
-				captionPanel.add(colorMenuBar);
-				captionPanel.add(captionLblR);
-				captionPanel.add(captionTxtR);
-				captionPanel.add(captionLblG);
-				captionPanel.add(captionTxtG);
-				captionPanel.add(captionLblB);
-				captionPanel.add(captionTxtB);
-				captionPanel.add(captionError);
-				captionPanel.add(changeColorBtn);
-				captionDialog.add(captionPanel);
-				captionDialog.add(captionPanel);
-				captionDialog.setSize(800, 800);
+				captionDialog.add(captionPanel, BorderLayout.CENTER);
+				captionDialog.add(colorPanel, BorderLayout.PAGE_END);
+				captionDialog.setSize(900, 600);
 				captionDialog.setVisible(true);
-				
+
+				// Btn action Listeners
 				menu0.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						index = 0;
@@ -670,7 +681,7 @@ public class GUI extends JFrame {
 						captionTxtB.setText("" + toni[index].getBlue());
 					}
 				});
-				
+
 				menu1.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						index = 1;
@@ -679,7 +690,7 @@ public class GUI extends JFrame {
 						captionTxtB.setText("" + toni[index].getBlue());
 					}
 				});
-				
+
 				menu2.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						index = 2;
@@ -688,7 +699,7 @@ public class GUI extends JFrame {
 						captionTxtB.setText("" + toni[index].getBlue());
 					}
 				});
-				
+
 				menu3.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						index = 3;
@@ -697,7 +708,7 @@ public class GUI extends JFrame {
 						captionTxtB.setText("" + toni[index].getBlue());
 					}
 				});
-				
+
 				menu4.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						index = 4;
@@ -706,11 +717,11 @@ public class GUI extends JFrame {
 						captionTxtB.setText("" + toni[index].getBlue());
 					}
 				});
-				
+
 				changeColorBtn.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						if(index<0) {
-							captionError.setText("Seleziona un range");
+							captionError.setText("Selezionare un range");
 							return;
 						}
 						captionTxtR.setBorder(new JTextField().getBorder());
@@ -733,13 +744,14 @@ public class GUI extends JFrame {
 						} catch (NumberFormatException nfe) {
 							captionTxtB.setBorder(new LineBorder(Color.red, 1));
 						}
-						if(r>=0 && g>=0 && b>=0 && r<=255 && g<=255 && b<=255) {
+						if (r >= 0 && g >= 0 && b >= 0 && r <= 255 && g <= 255 && b <= 255) {
 							Color nuovoColore = new Color(r, g, b);
-							if(nuovoColore.equals(Color.white) || nuovoColore.equals(Color.gray) || nuovoColore.equals(Color.darkGray) || nuovoColore.equals(Color.black)) {
-								captionError.setText("Non si accettano i colori bianco, grigio, grigio scuro e nero perché già usati altrove nell'applicazione");
+							if (nuovoColore.equals(Color.white) || nuovoColore.equals(Color.gray) || nuovoColore.equals(Color.darkGray) || nuovoColore.equals(Color.black)) {
+								captionError.setText(
+										"Non si accettano i colori bianco, grigio, grigio scuro e nero perché già usati altrove nell'applicazione");
 							} else {
-								for(int count = 0; count < 5; count++) {
-									if(toni[index] == nuovoColore) {
+								for (int count = 0; count < 5; count++) {
+									if (toni[index] == nuovoColore) {
 										captionError.setText("Colore già presente");
 										return;
 									}
